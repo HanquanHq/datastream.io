f4aa30d57578bd3d58a5836d7cdfbf58e3daa080
hanquanhq <15801016967@163.com>
2023-10-02 21:05:39 +0800

    gen data

--- examples/kafka-procuder-inf.py
+++ examples/kafka-procuder-inf.py
@@ -9,15 +9,18 @@ producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
 
 current_time = int(time.time())
 i = 0
-
+anomaly_cnt = 0
 while True:
     try:
-        if i % 20 == 0:
-            random_speed = random.randint(0, 1000)
-        elif i % 37 == 0:
-            random_speed = random.randint(200, 300)
+        if anomaly_cnt > 0:
+            anomaly_cnt = anomaly_cnt - 1;
+            random_speed = random.randint(100, 1000)
         else:
-            random_speed = random.randint(0, 100)
+            if i % random.randint(30, 40) == 0:
+                random_speed = random.randint(0, 1000)
+                anomaly_cnt = random.randint(0, 6)
+            else:
+                random_speed = random.randint(0, 300)
 
         message = {"time": current_time + i, "speed": random_speed}
         print('sending {}'.format(message))
